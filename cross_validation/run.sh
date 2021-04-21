06ee52d27eb6a278e39ee1f88db26721562d25ae
Darshan Solanki <das968@nyu.edu>
2021-04-21 19:51:26 -0400

    Revert "Added holdout feature in run.sh file"
    This reverts commit 87d3540fd4e6130f90c54048e75d365c95d88571.

--- cross_validation/run.sh
+++ cross_validation/run.sh
@@ -5,6 +5,5 @@ export KFOLDS=5
 export TARGET_COLS='SalePrice LotArea'
 export MULTI_DELIMITER=' '
 export SAVE_PATH='input/data/train_single_col_regression_folds.csv'
-export HOLDOUT = 'holdout_20'
 
 python ./src/cross_validation.py
